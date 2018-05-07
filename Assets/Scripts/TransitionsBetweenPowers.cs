0239ac66b7e6b25bda13d05851919aa06a77c786
Jonathan Bouligny <jonathanbouligny@gmail.com>
2018-05-07 04:06:56 -0500

    Build3

--- Assets/Scripts/TransitionsBetweenPowers.cs
+++ Assets/Scripts/TransitionsBetweenPowers.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TransitionsBetweenPowers : MonoBehaviour {
 
@@ -17,8 +18,49 @@ public class TransitionsBetweenPowers : MonoBehaviour {
     public float distanceSolar;
     public float distanceWind;
 
+    public Image solarIndicator;
+    public Image windIndicator;
+    public Image damIndicator;
+    public Text solarInstructions;
+    public Text windInstructions;
+    public Text damInstructions;
+
     void Update () {
-		if(Vector3.Distance(damCamera.transform.position,player.transform.position) <= distanceDam && Input.GetKeyDown(KeyCode.Return))
+
+        if(Vector3.Distance(damCamera.transform.position, player.transform.position) <= distanceDam)
+        {
+            damIndicator.gameObject.SetActive(true);
+            damInstructions.gameObject.SetActive(true);
+        }
+        else
+        {
+            damIndicator.gameObject.SetActive(false);
+            damInstructions.gameObject.SetActive(false);
+        }
+
+        if (Vector3.Distance(solarCamera.transform.position, player.transform.position) <= distanceSolar)
+        {
+            solarIndicator.gameObject.SetActive(true);
+            solarInstructions.gameObject.SetActive(true);
+        }
+        else
+        {
+            solarIndicator.gameObject.SetActive(false);
+            solarInstructions.gameObject.SetActive(false);
+        }
+
+        if (Vector3.Distance(windCamera.transform.position, player.transform.position) <= distanceWind)
+        {
+            windIndicator.gameObject.SetActive(true);
+            windInstructions.gameObject.SetActive(true);
+        }
+        else
+        {
+            windIndicator.gameObject.SetActive(false);
+            windInstructions.gameObject.SetActive(false);
+        }
+
+        if (Vector3.Distance(damCamera.transform.position,player.transform.position) <= distanceDam && Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("test");
             player.SetActive(false);
